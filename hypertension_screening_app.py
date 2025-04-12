b66dcbc2d973e914f6432e0cf2135a672bd2c916
Joel Babatunde <babatundejoel25@gmail.com>
2025-04-12 15:35:00 +0200

    Update hypertension_screening_app.py

--- hypertension_screening_app.py
+++ hypertension_screening_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1yUvy9iqLQXnyK9jos2Onupo9gF7QBLGm
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import joblib
